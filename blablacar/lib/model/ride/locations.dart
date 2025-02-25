d16d2fad3defe0e3524e3412cdc78c0f7f918dee
gechleang <gechleanglysan1211@gmail.com>
2025-02-26 01:08:31 +0700

    BLA-004 - Implement Ride Preferences Form

--- blablacar/lib/model/ride/locations.dart
+++ blablacar/lib/model/ride/locations.dart
@@ -1,46 +1,49 @@
-///
-/// Enumation of available BlaBlaCar countries
-///
-enum Country {
-  france('France'),
-  uk('United Kingdom'),
-  spain('Spain');
+  ///
+  /// Enumation of available BlaBlaCar countries
+  ///
+  enum Country {
+    france('France'),
+    uk('United Kingdom'),
+    spain('Spain');
 
-  final String name;
+    final String name;
 
-  const Country(this.name);
-}
-
-///
-/// This model describes a location (city, street).
-///
-class Location {
-  final String name;
-  final Country country;
-
-  const Location({required this.name, required this.country});
-
-  @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) return true;
-    return other is Location && other.name == name && other.country == country;
+    const Country(this.name);
   }
 
-  @override
-  int get hashCode => name.hashCode ^ country.hashCode;
+  ///
+  /// This model describes a location (city, street).
+  ///
+  class Location {
+    final String name;
+    final Country country;
 
-  @override
-  String toString() {
-    return name;
+    const Location({required this.name, required this.country});
+
+    @override
+    bool operator ==(Object other) {
+      if (identical(this, other)) return true;
+      return other is Location && other.name == name && other.country == country;
+    }
+
+    @override
+    int get hashCode => name.hashCode ^ country.hashCode;
+
+    @override
+    String toString() {
+      return name;
+    }
+
+    // Get location through to ride_pref_form
+    static getAllLocations() {}
   }
-}
 
-///
-/// This model describes a street.
-///
-class Street {
-  final String name;
-  final Location city;
+  ///
+  /// This model describes a street.
+  ///
+  class Street {
+    final String name;
+    final Location city;
 
-  const Street({required this.name, required this.city});
-}
+    const Street({required this.name, required this.city});
+  }
